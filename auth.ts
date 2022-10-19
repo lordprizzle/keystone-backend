4693db6322b151e18b4c5d22d4154b2f0870200b
Paul Osah <lordprizzle@gmail.com>
2022-10-19 13:58:40 +0100

    keystone backend app

--- auth.ts
+++ auth.ts
@@ -1,11 +1,10 @@
-// auth.ts
 import { createAuth } from '@keystone-6/auth';
 import { statelessSessions } from '@keystone-6/core/session';
 import * as dotenv from 'dotenv';
 dotenv.config({path: '.env'})
 
 let sessionSecret = process.env.SESSION_SECRET || '';
-let sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
+let sessionMaxAge = 60 * 60 * 24 * 30;
 
 const session = statelessSessions({
   maxAge: sessionMaxAge,
